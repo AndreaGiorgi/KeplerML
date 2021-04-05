935c2efb03a89293db979caee404f10bf275c30f
Andrea Giorgi <andrea_giorgi@outlook.it>
2021-04-05 11:31:45 +0200

    Add TODO

--- src/ph_SVM_ANN.py
+++ src/ph_SVM_ANN.py
@@ -1,1 +1,7 @@
 # TODO
+# 1. Implement ETL pipeline
+# 2. Implement Feature selection algorithm
+# 3. Implement Feature cleaning, normalization and correction
+# 4. Implement Data visualization for analysis
+# 5. Implement ANN with 2/3 hidden layers and best practicies (Output layer logisitic regression since it's binary classification)
+# 6. Results visualization
--- src/ph_SVM_Polynomial.py
+++ src/ph_SVM_Polynomial.py
@@ -1,1 +1,5 @@
 # TODO
+# 1. Implement ETL pipeline
+# 2. Implement Feature selection algorithm
+# 3. Implement SVM model with Polynomial Kernel
+# 4. Results visualization
